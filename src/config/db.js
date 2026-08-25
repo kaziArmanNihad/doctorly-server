46d68f38da38eb91e3947f820010cb643ef6ca27
kazi arman <kaziarman@proton.me>
2026-08-25 15:38:28 +0600

    optimice db

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     );
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
+    // process.exit(1);
   }
 };
 
